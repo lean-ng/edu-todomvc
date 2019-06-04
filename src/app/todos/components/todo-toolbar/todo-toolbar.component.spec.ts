a9698dbf939fda74105b971fc09c05df878aac3a
Micha Alt <mail@lean-stack.de>
2019-06-04 15:36:05 +0200

    Starting point. Enjoy the ride.

---
+++ src/app/todos/components/todo-toolbar/todo-toolbar.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TodoToolbarComponent } from './todo-toolbar.component';
+
+describe('TodoToolbarComponent', () => {
+  let component: TodoToolbarComponent;
+  let fixture: ComponentFixture<TodoToolbarComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TodoToolbarComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TodoToolbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/todos/components/todo-toolbar/todo-toolbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'todo-toolbar',
+  templateUrl: './todo-toolbar.component.html',
+  styleUrls: ['./todo-toolbar.component.css']
+})
+export class TodoToolbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
