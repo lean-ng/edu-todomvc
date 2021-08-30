aebf15b155076e4af9ac52a4263a8f783e2575c3
Micha Alt <mail@lean-stack.de>
2021-08-30 22:14:37 +0200

    Fix some lint errors.

--- src/app/components/todos-input/todos-input.component.ts
+++ src/app/components/todos-input/todos-input.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'todos-input',
   templateUrl: './todos-input.component.html',
-  styleUrls: ['./todos-input.component.css']
+  styleUrls: ['./todos-input.component.css'],
 })
-export class TodosInputComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class TodosInputComponent {
+  constructor() {}
 }
--- src/app/components/todos-shell/todos-shell.component.ts
+++ src/app/components/todos-shell/todos-shell.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'todos-shell',
   templateUrl: './todos-shell.component.html',
-  styleUrls: ['./todos-shell.component.css']
+  styleUrls: ['./todos-shell.component.css'],
 })
-export class TodosShellComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class TodosShellComponent {
+  constructor() {}
 }
